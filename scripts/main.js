72208d365cf122339c7bbec77da920f23c9f6a80
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-10-28 19:40:53 +0000

    added animation to nav list items

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
     $('.nav-toggle').click(function(){
         $('.nav-toggle').toggleClass('active');
         $('.menu-list').toggleClass('menu-list-visible', 1000);
+        $('.menu-list-item').each(function(i) {
+            setTimeout(function() {
+              $('.menu-list-item').eq(i).toggleClass('slide-in');
+            }, (700 * (Math.exp(i * 0.2))) - 700);
+          });
     })
 });
