2df7f1b6caa849f53dccc700f6f1d137b2ef60c8
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-10-28 20:02:45 +0000

    adjustments to navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.nav-toggle').click(function(){
         $('.nav-toggle').toggleClass('active');
-        $('.menu-list').toggleClass('menu-list-visible', 1000);
+        $('.menu-list').toggleClass('menu-list-visible', 500);
         $('.menu-list-item').each(function(i) {
             setTimeout(function() {
               $('.menu-list-item').eq(i).toggleClass('slide-in');
