a97e1c55e48064241ba36bc4a83746eee243ac97
mineshkothari <30318792+mineshkothari@users.noreply.github.com>
2018-12-30 14:22:02 +0000

    JS cleanup - ready for submission

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     // NAVIGATION TOGGLE
     $('.nav-toggle').click(function(){
+        // EXECUTE FOLLOWING COMMANDS WHEN NAV TOGGLE IS CLICKED
         $('.nav-toggle').toggleClass('active');
         $('.menu-list').toggleClass('menu-list-visible', 500);
+        // SLIDE IN EACH LIST ITEM INDEPENDENTLY
         $('.menu-list-item').each(function(i) {
             setTimeout(function() {
               $('.menu-list-item').eq(i).toggleClass('slide-in');
@@ -12,8 +14,10 @@ $(document).ready(function(){
 
     // NAV LINK CLICK EVENT
     $('.nav-link').click(function() {
+        // EXECUTE FOLLOWING COMMANDS WHEN NAV LINK IS CLICKED
         $('.nav-toggle').removeClass('active');
         $('.menu-list').removeClass('menu-list-visible', 200);
+        // SLIDE OUT LIST ITEMS WHEN A LINK IS CLICKED
         $('.menu-list-item').each(function(i) {
             setTimeout(function() {
               $('.menu-list-item').eq(i).removeClass('slide-in');
@@ -42,6 +46,7 @@ $(document).ready(function(){
         });
         // PARALLAXING SUMMARY BOX
         $('.summary--parallax').each(function() {
+            // SET VARIABLES
             var currentWidth = $(window).width();
             var wScroll = $(window).scrollTop();
             var elemPos = $(this).parent().offset().top - 450;
@@ -49,11 +54,14 @@ $(document).ready(function(){
             var offsetInEm = scrollOffset * 0.01;
 
             if ( currentWidth <= 767 ) {
+                // NO PARALLAX ON SMALL SCREENS
                 $(this).css('top', 0+'em');
             } else if ( currentWidth >= 768 && currentWidth <= 959 ) {
+                // SET STARTING POINT/END POINT OF PARALLAX MOTION ON MD SCREENS
                 var limitedOffsetInEm = Math.max(Math.min(offsetInEm, 4), -8);
                 $(this).css('top', limitedOffsetInEm+'em');
             } else if ( currentWidth >= 960 ) {
+                // SET STARTING POINT/END POINT OF PARALLAX MOTION ON LG SCREENS
                 var limitedOffsetInEm = Math.max(Math.min(offsetInEm, 4), -6);
                 $(this).css('top', limitedOffsetInEm+'em');
             }
